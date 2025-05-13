77363409b74aaa3865cc4bcbf9e3ad7553c40bd2
camapulya73 <profil73ul@gmail.com>
2025-05-13 14:25:19 +0400

    Часть 3

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -5,28 +5,14 @@ namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // 
-    // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
-    // 
-    // GET: /HelloWorld/Welcome/ 
-    public string Welcome1()
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return "This is the Welcome action method...";
-    }
-    // GET: /HelloWorld/Welcome/ 
-    // Requires using System.Text.Encodings.Web;
-    public string Welcome2(string name, int numTimes = 1)
-    {
-        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-    }
-    // GET: /HelloWorld/Welcome/ 
-    // Requires using System.Text.Encodings.Web;
-    public string Welcome3(string name, int ID = 1)
-    {
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
