4f9beb5a3f886d13761651e0c809ca98b1bcbe1a
camapulya73 <barlly2007@mail.ru>
2025-05-27 13:52:18 +0300

    Часть 8

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -12,6 +12,7 @@ public class Movie
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
     public decimal Price { get; set; }
+    public string? Rating { get; set; }
 }
 // GET: Movies/Edit/5
 
